d250427f8c3af6f42e98a63d42ab17526773c39f
Holden Profit <chobits00_01@yahoo.com>
2016-03-30 22:46:05 -0400

    Initial checkin

---
+++ Assets/Scipts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float velocity = 1.0f;
+    public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        direction = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+        direction.Normalize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Move the ball according to it's direction and velocity
+        Vector3 deltaDirection = direction * velocity * Time.deltaTime;
+        transform.Translate(deltaDirection);
+    }
+
+    /// <summary>
+    ///     Given a Vector3 representing the normal of whatever surface this ball
+    ///     collided with, returns the previous direction vector mirrored around the normal
+    /// </summary>
+    /// <param name="wallNormal">Vector3 - Normal of surface the ball collided with</param>
+    /// <returns>Vector3 - Mirror of the direction property around the given wallNormal</returns>
+    Vector3 Bounce(Vector3 wallNormal) {
+        float dot = Vector3.Dot(direction, wallNormal);
+        Vector3 bouncedDireciton = direction - 2 * dot * wallNormal;
+        return bouncedDireciton.normalized;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Wall") {
+            foreach (ContactPoint contact in collision.contacts) {
+                direction = Bounce(contact.normal);
+            }
+        }
+        else if (collision.gameObject.tag == "Paddle") {
+            foreach (ContactPoint contact in collision.contacts) {
+                direction = Bounce(contact.normal);
+            }
+        }
+    }
+}
---
+++ Assets/Scipts/Paddle.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+    // Simple storage for which player this is, left or right (flips which controls to use)
+    public bool isLeftPlayer = false;
+    public float velocity = 5.0f;
+    // Direction is stored publicly so that Ball.cs can access it
+    public Vector3 direction;
+    // Represents the state of the user's input, whether they have moved the paddle or not
+    private bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+        direction = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move the paddle according to it's direction and velocity
+        Vector3 deltaDirection = direction * velocity * Time.deltaTime;
+        transform.Translate(deltaDirection);
+    }
+
+    /// <summary>
+    ///     Sets direction to an "up" 
+    /// </summary>
+    void MoveUp() {
+        direction = Vector3.forward;
+    }
+
+    /// <summary>
+    ///     Sets direction to an "down"
+    /// </summary>
+    void MoveDown () {
+        direction = Vector3.back;
+    }
+
+    /// <summary>
+    ///     Sets direction to a zero Vector3
+    /// </summary>
+    void StopMovement () {
+        direction = Vector3.zero;
+    }
+
+    /// <summary>
+    ///     Given an Event object of EventType KeyDown, checks against the assigned movement keys
+    ///     for either the "Left" or "Right" player
+    /// </summary>
+    /// <param name="e">Event with EventType of KeyDown</param>
+    void HandleKeyPress (Event e) {
+        if (isLeftPlayer) {
+            if (e.keyCode == KeyCode.W) {
+                MoveUp();
+            }
+            if (e.keyCode == KeyCode.S) {
+                MoveDown();
+            }
+        }
+        else {
+            if (e.keyCode == KeyCode.UpArrow) {
+                MoveUp();
+            }
+            if (e.keyCode == KeyCode.DownArrow) {
+                MoveDown();
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Given an Event object of EventType KeyUp, checks against the assigned movement keys
+    ///     for either the "Left" or "Right" player
+    /// </summary>
+    /// <param name="e">Event with EventType of KeyUp</param>
+    void HandleKeyRelease (Event e) {
+        if (isLeftPlayer) {
+            if (e.keyCode == KeyCode.W || e.keyCode == KeyCode.S) {
+                StopMovement();
+            }
+        }
+        else {
+            if (e.keyCode == KeyCode.UpArrow || e.keyCode == KeyCode.DownArrow) {
+                StopMovement();
+            }
+        }
+    }
+
+    void OnGUI() {
+        Event e = Event.current;
+
+        if (e.isKey) {
+            if (e.type == EventType.KeyDown) {
+                HandleKeyPress(e);
+            }
+            else if (e.type == EventType.KeyUp) {
+                HandleKeyRelease(e);
+            }
+        }
+    }
+}
